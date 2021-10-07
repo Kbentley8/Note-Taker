521946400ec885bb71fdab0a7de89a0877b17c58
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-07 11:27:45 -0400

    edited server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// Set up requirements
+const express =require("express");
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
+
+
+// Set up reading and writing files
+const readFile =util.promisify(fs.readFile);
+const writefile =util.promisify(fs.writeFile);
+
+
+//Setting up Express
+const app =express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({
+    extended: true
+}
