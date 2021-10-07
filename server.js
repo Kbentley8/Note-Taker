274df3fe0fad343e86fefe7002ce2a54bdd38ad8
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-07 14:21:11 -0400

    added comments

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const express= require('express');
 // Point Server to route files 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+// Create an express server
+const app = express();
+
+// Setting PORT
+const PORT = process.env.PORT || 3001;
+
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON data
+app.use(express.json());
