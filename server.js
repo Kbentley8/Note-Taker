3fb5dc3b542887e5fd147b2258802bef0071c4af
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-07 11:35:46 -0400

    edited server.js

--- server.js
+++ server.js
@@ -36,3 +36,61 @@ app.get("/api/notes", async function (req, res) {
     const noteData = await getNotes();
     return res.status(200).json(noteData);
 });
+
+
+// API POST Route
+app.post("/api/notes", async function (req, res) {
+    let newNote = req.body;
+    const noteData = await getNotes();
+
+    noteData.push({
+        ...newNote,
+        id: noteData.length + 1
+    });
+    await writeNotes(noteData);
+    return res.sendStatus(201);
+});
+
+// API DELETE Route
+app.delete("/api/notes/:id", async function (req, res) {
+    let noteID = Number(req.params.id);
+    const noteData = await getNotes();
+
+    const newNotes = noteData.reduce((acc, curr) => {
+        if (curr.id !== noteID) acc.push(curr);
+        return acc;
+    }, []);
+
+    await writeNotes(newNotes);
+    return res.sendStatus(200);
+});
+
+// Read function
+const getNotes = async () => {
+    try {
+        const data = await readFile(path.join(__dirname, './db/db.json'), 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
+
+// Write function
+const writeNotes = async (data) => {
+    try {
+        const update = await writeFile(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(data)
+        );
+        return update;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
+
+// Initiate Listener to start server
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
