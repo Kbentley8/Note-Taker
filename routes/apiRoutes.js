eb8c3a60670d55ac1602e1d99882b15b5650bf5f
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-07 14:00:49 -0400

    edited apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,3 +19,14 @@ router.post('/notes', (req, res) => {
         .catch(err => res.status(500).json(err));
 
 });
+
+// Bonus - DELETE request
+router.delete('/notes/:id', function (req, res) {
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = router;
